80ea9997303ae47d04819d1605d1212e18a3f373
outerovitch-maths <outero.co@gmail.com>
2024-09-14 14:46:07 +0000

    .sh comment

--- generate_indexes.sh
+++ generate_indexes.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+## DELETE JUNK FILES ##
+
+# Define the directory to start searching from (default is the current directory)
+DIR=${1:-.}
+
+# List of extensions considered as junk files
+EXTENSIONS=("aux" "log" "out" "toc" "synctex.gz" "fls" "fdb_latexmk")
+
+# Loop through each extension and delete the files
+for ext in "${EXTENSIONS[@]}"; do
+    find "$DIR" -type f -name "*.$ext" -exec rm -f {} +
+done
+
+echo "Deleted junk files"
+
+## CREATE SUB-LEVELS INDEXES ##
+
 # Initialize an empty array
 dir_array=()
 
@@ -9,34 +26,23 @@ for dir in */; do
     dir_array+=("${dir%/}")
 done
 
-# Define the directory to start searching from (default is the current directory)
-DIR=${1:-.}
-
-# List of extensions you consider as "junk" files
-EXTENSIONS=("aux" "log" "out" "toc" "synctex.gz" "fls" "fdb_latexmk")
-
-# Loop through each extension and delete the files
-for ext in "${EXTENSIONS[@]}"; do
-    find "$DIR" -type f -name "*.$ext" -exec rm -f {} +
-done
-
-echo "Deleted junk files!"
-
 # Create index for each level
 for dir in "${dir_array[@]}"; do
     cd "$dir"
-    tree -H '.' -L 2 --noreport --dirsfirst -T "Cours de Maths/$dir" --charset utf-8 -I "index.html" -I "*.tex" -o index.html
+    tree -H '.' -L 2 --noreport --dirsfirst -T "Cours de Maths/$dir" --charset utf-8 -I "index.html" -I "*.tex" -o index.html # Tree make sub-levels indexes
     sleep 0.5
-    sed -i 's|<a href="././">.</a><br>|<a href="https://outerovitch-maths.github.io/">↰</a><br>|' index.html
+    sed -i 's|<a href="././">.</a><br>|<a href="https://outerovitch-maths.github.io/">↰</a><br>|' index.html # Make href .. point to top-level index
     cd ../
     echo "Created index: $dir"
 done
 
-tree -H '.' -L 1 --noreport -T 'Cours de Maths/Choix du niveau' -d --charset utf-8 -o index.html
+## CREATE TOP-LEVEL INDEX ##
+
+tree -H '.' -L 1 --noreport -T 'Cours de Maths/Choix du niveau' -d --charset utf-8 -o index.html # Tree make top-level index
 sleep 0.5
-sed -i 's|<a href="./\([^"]*\)/">|\<a href="./\1/index.html">|g' index.html
+sed -i 's|<a href="./\([^"]*\)/">|\<a href="./\1/index.html">|g' index.html # Make href point to sub-indexes
 sleep 0.5
-sed -i '/<head>/a<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />' index.html
+sed -i '/<head>/a<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />' index.html # Add the sum icon to HTML head
 echo "Created general index"
 
 echo "All indexes created successfully."
