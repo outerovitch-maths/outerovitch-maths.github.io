9fee12c67d7a0402f9e8917ac8588395a72c3fc9
outerovitch-maths <outero.co@gmail.com>
2024-09-19 20:34:54 +0000

    1ere FA

--- generate_indexes.sh
+++ generate_indexes.sh
@@ -30,7 +30,7 @@ done
 for dir in "${dir_array[@]}"; do
     cd "$dir"
     tree -H '.' -L 2 --noreport --dirsfirst -T "Cours de Maths/$dir" --charset utf-8 -I "index.html" -I "*.tex" -o index.html # Tree make sub-levels indexes
-    sleep 0.5
+    sleep 0.25
     sed -i 's|<a href="././">.</a><br>|<a href="https://outerovitch-maths.github.io/">↰</a><br>|' index.html # Make href .. point to top-level index
     sed -i '/<p class="VERSION">/,/<\/p>/d' index.html # Remove the version information block
     cd ../
@@ -40,11 +40,11 @@ done
 ## CREATE TOP-LEVEL INDEX ##
 
 tree -H '.' -L 1 --noreport -T 'Cours de Maths' -d --charset utf-8 -o index.html # Tree make top-level index
-sleep 0.5
+sleep 0.25
 sed -i 's|<a href="./\([^"]*\)/">|\<a href="./\1/index.html">|g' index.html # Make href point to sub-indexes
-sleep 0.5
+sleep 0.25
 sed -i '/<head>/a<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />' index.html # Add the sum icon to HTML head
 sed -i '/<p class="VERSION">/,/<\/p>/d' index.html # Remove the version information block
 echo "Created general index"
 
-echo "All indexes created successfully."
+echo "All indexes successfully created."
